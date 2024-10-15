f91b3cbe84f2d03d2ec32c2b5b8becdeea452bed
zero <zero180t@qq.com>
2024-10-15 22:05:01 +0800

    Site updated: 2024-10-15 22:05:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/01/hello-world/"];function toRandomPost(){
+var posts=["2024/08/01/hello-world/","2024/10/15/成人高考 专升本 常考题型/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
